53e5f861609eae0298086da13d847fe41b901b2e
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-03-06 15:18:00 +0300

    Update email.ts

--- client/src/utilities/validators/form/special/email.ts
+++ client/src/utilities/validators/form/special/email.ts
@@ -2,9 +2,6 @@ import errors from "../errors";
 import generic from "../generic";
 
 const email = (val: string) =>
-	generic.isEmpty.string(
-		val,
-		() => !/^\S+@\S+$/.test(val.trim()) && errors.isInvalid("email")
-	);
+	generic.isEmpty.string(val, () => !/^\S+@\S+\.\S+$/.test(val.trim()) && errors.isInvalid("email"));
 
 export default email;
